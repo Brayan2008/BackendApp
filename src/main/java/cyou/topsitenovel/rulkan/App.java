562dc1016b9bb8c64b23076e5540edd4cd2e88f5
Brayam <brayamaristafrndz@gmail.com>
2025-05-29 19:45:12 -0500

    Update App.java
    Si

--- src/main/java/cyou/topsitenovel/rulkan/App.java
+++ src/main/java/cyou/topsitenovel/rulkan/App.java
@@ -2,6 +2,6 @@ package cyou.topsitenovel.rulkan;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Paco se la come al Piña");
     }
 }
